c2801c02032964fad20a263ae3d781f1f6fa4cbc
andrew-matt <andrew.matt@ya.ru>
2022-06-12 14:32:43 +0300

    add Footer component

---
+++ src/footer/Footer.js
@@ -0,0 +1,23 @@
+import style from './Footer.module.css'
+import styleContainer from "../common/styles/Container.module.css";
+
+function Footer() {
+    return (
+        <div className={style.footerBlock}>
+            <div className={`${styleContainer.container} ${style.footerContainer}`}>
+                <h2 className={style.title}>Andrei Matviychuk</h2>
+                <div className={style.footer}>
+                    <div className={style.footerSocial}></div>
+                    <div className={style.footerSocial}></div>
+                    <div className={style.footerSocial}></div>
+                    <div className={style.footerSocial}></div>
+                </div>
+                <div className={style.link}>
+                    © 2022 All rights reserved
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
