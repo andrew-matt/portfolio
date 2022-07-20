013b5f7338ba9cc903e9eedfb65050367d794f61
andrew-matt <andrew.matt@ya.ru>
2022-07-20 21:32:55 +0300

    update

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -2,16 +2,19 @@ import style from './Project.module.scss';
 
 function Project(props) {
     return (
-        <a className={style.projectContainer}>
-            <div className={style.imgPlus}>
-                <div className={style.imageContainer} style={props.style}>
+        <div className={style.container}>
+            <a>
+                <div className={style.project}>
+                    <div className={style.imageWrapper}>
+                        <img src={props.image} className={style.image}/>
+                    </div>
+                    <div className={style.content}>
+                        <div className={style.title}>{props.title}</div>
+                        <div className={style.description}>{props.description}</div>
+                    </div>
                 </div>
-            </div>
-            <div className={style.descriptionContainer}>
-                <div className={style.title}>{props.title}</div>
-                <div className={style.description}>{props.description}</div>
-            </div>
-        </a>
+            </a>
+        </div>
     );
 }
 
