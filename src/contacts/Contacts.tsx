a3bf1845e6915ac6e16d3b70b2fcb391af5cf985
andrew-matt <andrew.matt@ya.ru>
2022-09-29 10:28:40 +0300

    add react reveal and react typing effect

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -13,6 +13,8 @@ import { Button } from 'common/components/button/Button';
 import { SnackBar } from 'common/components/snackbar/SnackBar';
 import { Title } from 'common/components/title/Title';
 
+const Fade = require('react-reveal/Fade');
+
 export const Contacts: FC = () => {
   const [sendingMessageStatus, setSendingMessageStatus] = useState(false);
   const [snackBarOpen, setSnackBarOpen] = useState(false);
@@ -60,54 +62,62 @@ export const Contacts: FC = () => {
       <div className={style.block}>
         <div className={style.container}>
           <Title title="Contact With Me" />
-          <form className={style.form} spellCheck="false" onSubmit={formik.handleSubmit}>
-            <div className={style.wrapper}>
-              <span className={style.title}>your name</span>
-              <input
-                type="text"
-                className={style.input}
-                {...formik.getFieldProps('name')}
-              />
-              <div className={style.error}>
-                {formik.touched.name && formik.errors.name ? formik.errors.name : null}
+          <Fade left>
+            <form
+              className={style.form}
+              spellCheck="false"
+              onSubmit={formik.handleSubmit}
+            >
+              <div className={style.wrapper}>
+                <span className={style.title}>your name</span>
+                <input
+                  type="text"
+                  className={style.input}
+                  {...formik.getFieldProps('name')}
+                />
+                <div className={style.error}>
+                  {formik.touched.name && formik.errors.name ? formik.errors.name : null}
+                </div>
               </div>
-            </div>
-            <div className={style.wrapper}>
-              <span className={style.title}>your email</span>
-              <input
-                type="text"
-                className={style.input}
-                {...formik.getFieldProps('email')}
-              />
-              <div className={style.error}>
-                {formik.touched.email && formik.errors.email ? formik.errors.email : null}
+              <div className={style.wrapper}>
+                <span className={style.title}>your email</span>
+                <input
+                  type="text"
+                  className={style.input}
+                  {...formik.getFieldProps('email')}
+                />
+                <div className={style.error}>
+                  {formik.touched.email && formik.errors.email
+                    ? formik.errors.email
+                    : null}
+                </div>
               </div>
-            </div>
-            <div className={style.wrapper}>
-              <span className={style.title}>your message</span>
-              <textarea
-                cols={30}
-                rows={10}
-                className={style.textarea}
-                {...formik.getFieldProps('message')}
-              />
-              <div className={style.error}>
-                {formik.touched.message && formik.errors.message
-                  ? formik.errors.message
-                  : null}
+              <div className={style.wrapper}>
+                <span className={style.title}>your message</span>
+                <textarea
+                  cols={30}
+                  rows={10}
+                  className={style.textarea}
+                  {...formik.getFieldProps('message')}
+                />
+                <div className={style.error}>
+                  {formik.touched.message && formik.errors.message
+                    ? formik.errors.message
+                    : null}
+                </div>
               </div>
-            </div>
-            {sendingMessageStatus ? (
-              <div className={style.loadingWrapper}>
-                <CircularProgress className={style.loadingCircular} />
-                <span className={style.loadingMessage}>Sending message...</span>
-              </div>
-            ) : (
-              <div>
-                <Button title="SEND MESSAGE" />
-              </div>
-            )}
-          </form>
+              {sendingMessageStatus ? (
+                <div className={style.loadingWrapper}>
+                  <CircularProgress className={style.loadingCircular} />
+                  <span className={style.loadingMessage}>Sending message...</span>
+                </div>
+              ) : (
+                <div>
+                  <Button title="SEND MESSAGE" />
+                </div>
+              )}
+            </form>
+          </Fade>
         </div>
       </div>
       <SnackBar
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,26 +1,35 @@
 import { FC } from 'react';
 
+import ReactTypingEffect from 'react-typing-effect';
+
 import style from './Main.module.scss';
 import Photo from './Photo.png';
 
+const Fade = require('react-reveal/Fade');
+
 export const Main: FC = () => {
   const mainDescription =
-    'a Front-end Developer. Nec ullamcorper sit amet risus nullam eget felis. Felis eget nunc lobortis mattis aliquam faucibus. Ullamcorper malesuada proin libero nunc. Purus faucibus ornare suspendisse sed nisi lacus sed. Pellentesque elit eget gravida cum.';
+    'Nec ullamcorper sit amet risus nullam eget felis. Felis eget nunc lobortis mattis aliquam faucibus. Ullamcorper malesuada proin libero nunc. Purus faucibus ornare suspendisse sed nisi lacus sed. Pellentesque elit eget gravida cum.';
 
   return (
     <div className={style.mainBlock}>
-      <div className={style.container}>
-        <div className={style.greeting}>
-          <h1>
-            <span>Hi, I&apos;m</span>
-            <span> Andrei Matviychuk</span>
-          </h1>
-          <p>{mainDescription}</p>
+      <Fade top>
+        <div className={style.container}>
+          <div className={style.greeting}>
+            <h1>
+              <span>Hi, I&apos;m</span>
+              <span> Andrei Matviychuk</span>
+              <div>
+                <ReactTypingEffect text="a Front-end Developer" />
+              </div>
+            </h1>
+            <p>{mainDescription}</p>
+          </div>
+          <div className={style.photo}>
+            <img src={Photo} width="100%" alt="me" />
+          </div>
         </div>
-        <div className={style.photo}>
-          <img src={Photo} width="100%" alt="me" />
-        </div>
-      </div>
+      </Fade>
     </div>
   );
 };
